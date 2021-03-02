c2dab3f46940d988b54a7182caa3655d1a8c285a
Xuan Linh Phamová <xphamov2@fi.muni.cz>
2021-03-02 11:11:40 +0100

    initial commit

---
+++ src/main/java/com/muni/java/pa165/App.java
@@ -0,0 +1,13 @@
+package com.muni.java.pa165;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
